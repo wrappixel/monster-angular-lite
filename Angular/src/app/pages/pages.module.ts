32ff7c2d17a2de784bae52b36151ff8eb57a09e9
vishal-wrap <31442586+vishal-wrap@users.noreply.github.com>
2018-09-05 12:53:38 +0530

    ''

--- Angular/src/app/pages/pages.module.ts
+++ Angular/src/app/pages/pages.module.ts
@@ -5,7 +5,6 @@ import { PageComponent } from './pages.component';
 import { SIDEBAR_TOGGLE_DIRECTIVES } from '../shared/sidebar.directive';
 import { NavigationComponent } from '../shared/header-navigation/navigation.component';
 import { SidebarComponent } from '../shared/sidebar/sidebar.component';
-import { BreadcrumbComponent } from '../shared/breadcrumb/breadcrumb.component';
 
 @NgModule({
     imports: [
@@ -15,7 +14,6 @@ import { BreadcrumbComponent } from '../shared/breadcrumb/breadcrumb.component';
     declarations: [
         PageComponent,
         NavigationComponent,
-        BreadcrumbComponent,
         SidebarComponent,
         SIDEBAR_TOGGLE_DIRECTIVES
     ]
