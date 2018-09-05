54f64cf954ad86ac9c70af7a6e35313d46a67bec
vishal-wrap <31442586+vishal-wrap@users.noreply.github.com>
2018-09-05 16:04:47 +0530

    TestApp

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -3,15 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
-    {
-        path: '',
-        loadChildren: './pages/pages.module#PagesModule'
-    },
-    { path: '**', redirectTo: '' }
+  {
+    path: '',
+    loadChildren: './pages/pages.module#PagesModule'
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes), NgbModule.forRoot()],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), NgbModule.forRoot()],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
